4fb26adac4ceabd75a4c983a342c1c4891b38cd0
Liu112-web <2728537061@qq.com>
2026-07-21 10:14:23 +0800

    Add frontend/src/App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted } from 'vue'
+import Dashboard from './views/Dashboard.vue'
+import { usePriceStore } from './stores/price'
+
+const store = usePriceStore()
+onMounted(() => {
+  store.fetchAll()
+})
+</script>
+
+<template>
+  <Dashboard />
+</template>
+
+<style scoped>
+</style>
